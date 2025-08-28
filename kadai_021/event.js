4fab58d2638fecbf54d22fed34782e34ed67bd10
inoue-s1 <inoue.s1@shinmaywa.co.jp>
2025-08-28 15:48:34 +0900

    changed

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,8 +1,8 @@
 const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
 	setTimeout(() => {
-		const text = document.getElementById('text');
 		text.textContent = 'ボタンをクリックしました';
 	}, 2000);
 });
