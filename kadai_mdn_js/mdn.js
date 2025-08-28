fc3b4143eab53644e61348abc87a30c2ded9f908
inoue-s1 <inoue.s1@shinmaywa.co.jp>
2025-08-28 11:18:18 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,3 @@
 const date = new Date();
-const [month, day, year] = [
-  date.getMonth()+1,
-  date.getDate(),
-  date.getFullYear(),
-];
 
-console.log(year+"年"+month+"月"+day+"日");
+console.log(date.getFullYear()+'年'+(date.getMonth()+1)+'月'+date.getDate()+'日');
